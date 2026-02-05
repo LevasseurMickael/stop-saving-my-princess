885ebec3b37d5720ddffe2b0ddd8c4b918aee1b8
Stwompy <stwompy@gmail.com>
2026-02-05 14:18:57 +0100

    feat collider, now ennemy box collide correctly with user one and kill the player

--- src/game/turn.ts
+++ src/game/turn.ts
@@ -1,6 +1,7 @@
 import { enemy } from "./enemy";
 import { map } from "./map";
 import { player } from "./player";
+import { state } from "./state";
 
 export function enemyTurn() {
   if (!enemy.alive) return;
@@ -22,5 +23,12 @@ export function enemyTurn() {
   if (map[newY][newX] !== 1) {
     enemy.x = newX;
     enemy.y = newY;
+  }
+
+  // collision with player
+  if (enemy.x === player.x && enemy.y === player.y) {
+    player.x = 1;
+    player.y = 1;
+    state.deathCount++;
   }
 }
