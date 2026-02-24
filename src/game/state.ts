aa96d137ccb3ed39b5291db63a95379cbf86642a
Stwompy <stwompy@gmail.com>
2026-02-24 09:13:17 +0100

    feature passives => now canHintWalls function is created, the stairs are hidden starting floor 20 unless the item was unlocked

--- src/game/state.ts
+++ src/game/state.ts
@@ -10,7 +10,7 @@ const MaxFloors = 50; // Maximum number of floors in the game, used for initiali
 
 // Dungeon related state, separated for clarity and potential future expansion
 export const stateDungeon = {
-  currentFloor: 26,
+  currentFloor: 20,
   floorState: "0".repeat(MaxFloors),
   runSeed: Date.now(), // Unique seed for each run
 };
--- src/graphicContext/mapContext.ts
+++ src/graphicContext/mapContext.ts
@@ -1,6 +1,7 @@
 // This file contains the function to render the map tiles based on the map data structure. It is imported in main.ts to keep the code organized and maintainable.
 
 import { canSeeExits } from "../game/secret/secretUnlock/itemEffect/passives/canSeeExits";
+import { canSeeHintWalls } from "../game/secret/secretUnlock/itemEffect/passives/canSeeHintWall";
 
 export function getMapSprite(
   ctx: CanvasRenderingContext2D,
@@ -18,7 +19,7 @@ export function getMapSprite(
       } else if (map[y][x] === 3) {
         ctx.fillStyle = canSeeExits();
       } else if (map[y][x] === 4) {
-        ctx.fillStyle = "white";
+        ctx.fillStyle = canSeeHintWalls();
       } else if (map[y][x] === 5) {
         ctx.fillStyle = "teal";
       } else if (map[y][x] === 6) {
